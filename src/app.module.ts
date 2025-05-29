ad3a68762ff5e65a8f7ebd7e027b4cfaa9b823b9
polismai <polismai@gmail.com>
2025-05-29 19:10:54 -0300

    initial setup with morgan, CORS, and ConfigService

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('dev'));
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors(CORS);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(configService.get('PORT'));
+}
+bootstrap();
