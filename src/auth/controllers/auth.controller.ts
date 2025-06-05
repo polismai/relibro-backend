d02ddc843af58b941a1373f9a4b8ef426b90450e
polismai <polismai@gmail.com>
2025-06-05 14:11:19 -0300

    add login endpoint with validation and typed response

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -2,12 +2,14 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { ErrorManager } from 'src/common/utils/error.manager';
 import { AuthDto } from '../dto/auth.dto';
+import { AuthResponse } from '../interfaces/auth.interface';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('login')
-  async login(@Body() { email, password }: AuthDto) {
+  async login(@Body() authDto: AuthDto): Promise<AuthResponse> {
+    const { email, password } = authDto;
     const user = await this.authService.validateUser(email, password);
 
     if (!user) {
--- src/auth/interfaces/auth.interface.ts
+++ src/auth/interfaces/auth.interface.ts
@@ -1,3 +1,5 @@
+import { User } from '../../users/entities/user.entity';
+
 export interface PayloadToken {
   sub: string;
   role: string;
@@ -6,4 +8,9 @@ export interface PayloadToken {
 export interface AuthBody {
   email: string;
   password: string;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  user: User;
 }
