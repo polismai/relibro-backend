75dc4f112f26dd9969227e43749cf79a6494cb18
polismai <polismai@gmail.com>
2025-07-24 14:37:40 -0300

    add school year enum and endpoint to expose available school years

---
+++ src/schools/school-years.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { enumToArray } from '../common/utils/enum-to-array';
+import { SchoolYear } from '../common/enums/school-years.enum';
+
+@Controller('school-years')
+export class SchoolYearsController {
+  @Get()
+  getSchoolYears(): string[] {
+    return enumToArray(SchoolYear);
+  }
+}
--- src/schools/schools.module.ts
+++ src/schools/schools.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SchoolsController } from './schools.controller';
+import { SchoolYearsController } from './school-years.controller';
 
 @Module({
-  controllers: [SchoolsController],
+  controllers: [SchoolsController, SchoolYearsController],
 })
 export class SchoolsModule {}
